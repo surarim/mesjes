0a0e81d65683b5870545b30f5a3f4bd7bc67ce2f
surarim <surarim@yandex.ru>
2018-08-14 16:49:05 +0700

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,38 @@
+<script>
+
+// Функция чтения сообщений
+message_read = setTimeout(function tick()
+    {
+    var req = new XMLHttpRequest();
+    req.open('POST', '/read', true);
+    req.onreadystatechange = function()
+        {
+	    if (req.readyState == XMLHttpRequest.DONE && req.status == 200)
+            {
+            if (req.responseText != "")
+                { document.getElementById("fullchat").innerHTML = req.responseText; }
+                else { document.getElementById("fullchat").innerHTML = ""; }
+            }
+        }
+    req.send();
+    //
+    message_read = setTimeout(tick, 1000);
+    }, 1000);
+
+// Функция отправки сообщения
+function message_send(event)
+    {
+    if (event.keyCode == 13)
+        {
+        message_text = document.getElementById("message").value;
+        document.getElementById("message").value = "";
+        var req = new XMLHttpRequest();
+        req.open('POST', '/add', true);
+        req.send(message_text+"<br>");
+        };
+    };
+
+// Функция загрузки
+//window.onload = function() { message_read() };
+
+</script>
