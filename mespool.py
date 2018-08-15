3898b554b9bb1f3e753659727f0624b949b1aa66
surarim <surarim@yandex.ru>
2018-08-15 11:39:34 +0700

    Update mespool.py

--- mespool.py
+++ mespool.py
@@ -1,17 +1,24 @@
+root_folder = '/var/www/'
 def application(env, start_response):
-    scripts = open('/var/www/scripts.js', 'r').read()
-    forms = open('/var/www/forms.html', 'r').read()
+    scripts = open(root_folder+'scripts.js', 'r').read()
+    forms = open(root_folder+'forms.html', 'r').read()
     if env['REQUEST_METHOD'] == 'POST':
         if env.get('CONTENT_LENGTH'): env_len = int(env.get('CONTENT_LENGTH'))
         else: env_len = 0
         if env_len > 0: post = env['wsgi.input'].read(env_len).decode('utf-8')
         else: post = ""
-        mes = ""
-        if env['PATH_INFO'] == "/read": mes = open('/var/www/messages','r').read()
-        if env['PATH_INFO'] == "/add": mes = open('/var/www/messages','a').write(post+'\n')
-        html = mes
+        result = ""
+        if env['PATH_INFO'] == "/read": result = open(root_folder+'messages','r').read()
+        if env['PATH_INFO'] == "/add": result = open(root_folder+'messages','a').write(post+'\n')
+        start_response('200 OK', [('Content-Type', 'text/html'), ('Content-Length', str(len(result)))])
+        yield result.encode('utf-8')
     else:
-        html = '<html><head>' + scripts + '</head><body>' + forms + '</body></html>'
-    start_response('200 OK', [('Content-Type', 'text/html'), ('Content-Length', str(len(html)))])
-    yield html.encode('utf-8')
-    
+        if env['PATH_INFO'] == "/favicon.ico":
+            favicon = open(root_folder+'mesjes.png', 'rb').read()
+            start_response('200 OK', [('Content-Type','image/png'),('Content-Length', str(len(favicon)))])
+            yield favicon
+        else:
+            html = '<html>\n<head>\n' + scripts + '\n</head>\n<body>\n' + forms + '\n</body>\n</html>'
+            start_response('200 OK', [('Content-Type', 'text/html'), ('Content-Length', str(len(html)))])
+            yield html.encode('utf-8')
+            
